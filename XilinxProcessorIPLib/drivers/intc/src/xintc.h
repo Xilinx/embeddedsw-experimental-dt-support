be042282abb6c07f7d3d15fa51fa51e156e8e00f
Appana Durga Kedareswara rao <appana.durga.rao@xilinx.com>
2021-01-23 20:10:48 +0530

    intc: Add support for system device-tree flow
    Signed-off-by: Appana Durga Kedareswara rao <appana.durga.rao@xilinx.com>

--- XilinxProcessorIPLib/drivers/intc/src/xintc.h
+++ XilinxProcessorIPLib/drivers/intc/src/xintc.h
@@ -217,8 +217,10 @@ extern "C" {
 
 #include "xil_types.h"
 #include "xil_assert.h"
-#include "xparameters.h"
 #include "xstatus.h"
+#ifndef SDT
+#include "xparameters.h"
+#endif
 #include "xintc_l.h"
 
 /************************** Constant Definitions *****************************/
@@ -278,7 +280,11 @@ extern "C" {
  * This typedef contains configuration information for the device.
  */
 typedef struct {
+#ifndef SDT
 	u16 DeviceId;		/**< Unique ID  of device */
+#else
+	char *Name;
+#endif
 	UINTPTR BaseAddress;	/**< Register base address */
 	u32 AckBeforeService;	/**< 0 - Interrupt would be acked before service through primary interrupt handler
 								 1 - Interrupt would be acked after service through primary interrupt handler */
@@ -338,7 +344,11 @@ void XIntc_Disable(XIntc * InstancePtr, u8 Id);
 
 void XIntc_Acknowledge(XIntc * InstancePtr, u8 Id);
 
+#ifndef SDT
 XIntc_Config *XIntc_LookupConfig(u16 DeviceId);
+#else
+XIntc_Config *XIntc_LookupConfig(UINTPTR BaseAddr);
+#endif
 
 int XIntc_ConnectFastHandler(XIntc *InstancePtr, u8 Id,
 				XFastInterruptHandler Handler);
