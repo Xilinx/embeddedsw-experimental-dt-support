37ddef0c94a435d80bd4bf489a1e856c4dd9e50e
Appana Durga Kedareswara rao <appana.durga.rao@xilinx.com>
2021-01-24 10:50:20 +0530

    sysmon: Add support for system device-tree flow
    Signed-off-by: Appana Durga Kedareswara rao <appana.durga.rao@xilinx.com>

--- XilinxProcessorIPLib/drivers/sysmon/src/xsysmon_sinit.c
+++ XilinxProcessorIPLib/drivers/sysmon/src/xsysmon_sinit.c
@@ -29,8 +29,10 @@
 
 /***************************** Include Files *********************************/
 
-#include "xparameters.h"
 #include "xsysmon.h"
+#ifndef SDT
+#include "xparameters.h"
+#endif
 
 /************************** Constant Definitions *****************************/
 
@@ -60,6 +62,7 @@ extern XSysMon_Config XSysMon_ConfigTable[];
 * @note		None.
 *
 ******************************************************************************/
+#ifndef SDT
 XSysMon_Config *XSysMon_LookupConfig(u16 DeviceId)
 {
 	XSysMon_Config *CfgPtr = NULL;
@@ -74,4 +77,21 @@ XSysMon_Config *XSysMon_LookupConfig(u16 DeviceId)
 
 	return CfgPtr;
 }
+#else
+XSysMon_Config *XSysMon_LookupConfig(u32 BaseAddress)
+{
+	XSysMon_Config *CfgPtr = NULL;
+	u32 Index;
+
+	for (Index=0; XSysMon_ConfigTable[Index].Name != NULL; Index++) {
+		if ((XSysMon_ConfigTable[Index].BaseAddress == BaseAddress)  ||
+			!BaseAddress) {
+			CfgPtr = &XSysMon_ConfigTable[Index];
+			break;
+		}
+	}
+
+	return CfgPtr;
+}
+#endif
 /** @} */
