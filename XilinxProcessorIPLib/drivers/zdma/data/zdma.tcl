df4a51756bc41c0f1b1011424984546b8c4f0fe7
Appana Durga Kedareswara rao <appana.durga.rao@xilinx.com>
2021-01-25 15:00:13 +0530

    zdma: Add support for interrupt wrapper API
    Signed-off-by: Appana Durga Kedareswara rao <appana.durga.rao@xilinx.com>

--- XilinxProcessorIPLib/drivers/zdma/data/zdma.tcl
+++ XilinxProcessorIPLib/drivers/zdma/data/zdma.tcl
@@ -25,9 +25,19 @@ proc generate {drv_handle} {
     ::hsi::utils::define_zynq_include_file $drv_handle "xparameters.h" "XZDma" "NUM_INSTANCES" "DEVICE_ID" "C_S_AXI_BASEADDR" "C_DMA_MODE" "C_S_AXI_HIGHADDR" "C_ZDMA_CLK_FREQ_HZ"
     set is_xen [common::get_property CONFIG.hypervisor_guest [hsi::get_os]]
     generate_cci_params $drv_handle "xparameters.h"
-    ::hsi::utils::define_zynq_config_file $drv_handle "xzdma_g.c" "XZDma"  "DEVICE_ID" "C_S_AXI_BASEADDR" "C_DMA_MODE" "IS_CACHE_COHERENT"
+    ::hsi::utils::define_zynq_config_file $drv_handle "xzdma_g.c" "XZDma"  "DEVICE_ID" "C_S_AXI_BASEADDR" "C_DMA_MODE" "IS_CACHE_COHERENT" "C_INTERRUPT" "C_INTR_PARENT"
 
     ::hsi::utils::define_zynq_canonical_xpars $drv_handle "xparameters.h" "XZDma" "DEVICE_ID" "C_S_AXI_BASEADDR" "C_DMA_MODE" "C_S_AXI_HIGHADDR" "C_ZDMA_CLK_FREQ_HZ"
+
+    foreach i [get_sw_cores standalone*] {
+        set intr_wrapper_tcl_file "[get_property "REPOSITORY" $i]/data/intr_wrapper.tcl"
+        if {[file exists $intr_wrapper_tcl_file]} {
+            source $intr_wrapper_tcl_file
+            break
+        }
+    }
+
+    gen_intr $drv_handle "xparameters.h"
 
 }
 proc generate_cci_params {drv_handle file_name} {
