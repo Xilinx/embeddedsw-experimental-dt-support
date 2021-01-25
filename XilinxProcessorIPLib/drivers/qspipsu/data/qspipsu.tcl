83d0317cfddbabcd95b86f2a7b9d51745f298d50
Appana Durga Kedareswara rao <appana.durga.rao@xilinx.com>
2021-01-25 15:04:15 +0530

    qspipsu: Add interrupt field to config structure
    Signed-off-by: Appana Durga Kedareswara rao <appana.durga.rao@xilinx.com>

--- XilinxProcessorIPLib/drivers/qspipsu/data/qspipsu.tcl
+++ XilinxProcessorIPLib/drivers/qspipsu/data/qspipsu.tcl
@@ -29,12 +29,13 @@ proc generate {drv_handle} {
 	set isclocking [check_clocking]
 
 	if { $isclocking == 1 &&  $is_zynqmp_fsbl_bsp != true   &&  [llength $cortexa53proc] > 0 && [string match -nocase $clocking "true"] > 0} {
-    ::hsi::utils::define_zynq_config_file $drv_handle "xqspipsu_g.c" "XQspiPsu"  "DEVICE_ID" "C_S_AXI_BASEADDR" "C_QSPI_CLK_FREQ_HZ" "C_QSPI_MODE" "C_QSPI_BUS_WIDTH" "IS_CACHE_COHERENT" "REF_CLK"
+    ::hsi::utils::define_zynq_config_file $drv_handle "xqspipsu_g.c" "XQspiPsu"  "DEVICE_ID" "C_S_AXI_BASEADDR" "C_QSPI_CLK_FREQ_HZ" "C_QSPI_MODE" "C_QSPI_BUS_WIDTH" "IS_CACHE_COHERENT" "REF_CLK" "C_INTERRUPT" "C_INTR_PARENT"
 	} else {
-    ::hsi::utils::define_zynq_config_file $drv_handle "xqspipsu_g.c" "XQspiPsu"  "DEVICE_ID" "C_S_AXI_BASEADDR" "C_QSPI_CLK_FREQ_HZ" "C_QSPI_MODE" "C_QSPI_BUS_WIDTH" "IS_CACHE_COHERENT"
+    ::hsi::utils::define_zynq_config_file $drv_handle "xqspipsu_g.c" "XQspiPsu"  "DEVICE_ID" "C_S_AXI_BASEADDR" "C_QSPI_CLK_FREQ_HZ" "C_QSPI_MODE" "C_QSPI_BUS_WIDTH" "IS_CACHE_COHERENT" "C_INTERRUPT" "C_INTR_PARENT"
 	}
     ::hsi::utils::define_zynq_canonical_xpars $drv_handle "xparameters.h" "XQspiPsu" "DEVICE_ID" "C_S_AXI_BASEADDR" "C_S_AXI_HIGHADDR" "C_QSPI_CLK_FREQ_HZ" "C_QSPI_MODE" "C_QSPI_BUS_WIDTH" "IS_CACHE_COHERENT"
 
+    gen_intr $drv_handle "xparameters.h"
 }
 
 proc check_clocking { } {
@@ -80,4 +81,56 @@ proc generate_cci_params {drv_handle file_name} {
 		}
 	}
 	close $file_handle
+}
+
+proc gen_intr {drv_handle file_name} {
+    set file_handle [::hsi::utils::open_include_file $file_name]
+    set ips [::hsi::utils::get_common_driver_ips $drv_handle]
+    set sw_processor [hsi::get_sw_processor]
+    set processor [hsi::get_cells -hier [common::get_property HW_INSTANCE $sw_processor]]
+    set processor_type [common::get_property IP_NAME $processor]
+
+    foreach ip $ips {
+        set isintr [::hsm::utils::is_ip_interrupting_current_proc $ip]
+        set intc_parent_addr 0xffff
+	set intr 0xffff
+	if {$isintr == 1} {
+            set intr_pin_name [hsi::get_pins -of_objects [hsi::get_cells -hier $ip]  -filter {TYPE==INTERRUPT&&DIRECTION==O}]
+	    foreach pin $intr_pin_name {
+		set intcname [::hsi::utils::get_connected_intr_cntrl $ip $pin]
+                if {[llength $intcname] == 0 || [string match $intcname "{}"] } {
+                    continue
+                }
+		foreach intc $intcname {
+		    set ipname [common::get_property IP_NAME $intc]
+		    if {$processor_type == "psu_cortexa53" && $ipname == "psu_acpu_gic"} {
+		        set intc_parent_addr [common::get_property CONFIG.C_S_AXI_BASEADDR $intc]
+		    }
+		    if {$processor_type == "psu_cortexr5" && $ipname == "psu_rcpu_gic"} {
+			set intc_parent_addr [common::get_property CONFIG.C_S_AXI_BASEADDR $intc]
+		    }
+		    if {$ipname == "axi_intc"} {
+			set intc_parent_addr [common::get_property CONFIG.C_BASEADDR $intc]
+			incr intc_parent_addr
+			set intc_parent_addr [format 0x%xU $intc_parent_addr]
+		    }
+		}
+            }
+            if {${processor_type} == "microblaze"} {
+		set intcname [string toupper $intcname]
+		set ip_name [string toupper $ip]
+		set intr_pin_name [string toupper $intr_pin_name]
+	        puts $file_handle "\#define [::hsi::utils::get_driver_param_name $ip "C_INTERRUPT"] XPAR_${intcname}_${ip_name}_${intr_pin_name}_INTR"
+	   } else {
+                set ip_name [string toupper $ip]
+	        puts $file_handle "\#define [::hsi::utils::get_driver_param_name $ip "C_INTERRUPT"] XPAR_${ip_name}_INTR"
+	   }
+	} else {
+	        puts $file_handle "\#define [::hsi::utils::get_driver_param_name $ip "C_INTERRUPT"] $intr"
+	}
+
+        puts $file_handle "\#define [::hsi::utils::get_driver_param_name $ip "C_INTR_PARENT"] $intc_parent_addr"
+    }
+
+    close $file_handle
 }
