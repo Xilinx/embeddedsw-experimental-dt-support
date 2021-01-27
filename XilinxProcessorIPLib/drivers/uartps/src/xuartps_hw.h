3d42fc078233904126150fc363d134fd932d7603
Appana Durga Kedareswara rao <appana.durga.rao@xilinx.com>
2021-01-27 11:42:18 +0530

    uartps: include xstatus.h header
    Signed-off-by: Appana Durga Kedareswara rao <appana.durga.rao@xilinx.com>

--- XilinxProcessorIPLib/drivers/uartps/src/xuartps_hw.h
+++ XilinxProcessorIPLib/drivers/uartps/src/xuartps_hw.h
@@ -48,6 +48,7 @@ extern "C" {
 #include "xil_types.h"
 #include "xil_assert.h"
 #include "xil_io.h"
+#include "xstatus.h"
 
 /************************** Constant Definitions *****************************/
 
