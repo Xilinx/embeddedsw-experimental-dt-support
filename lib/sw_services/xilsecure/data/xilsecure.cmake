c182d932b74dca33250685b03f81dec3f7615bb4
Appana Durga Kedareswara rao <appana.durga.rao@xilinx.com>
2021-01-28 14:33:22 +0530

    lib: sw_services: xilsecure: Add support for system device-tree flow
    Signed-off-by: Appana Durga Kedareswara rao <appana.durga.rao@xilinx.com>

---
+++ lib/sw_services/xilsecure/data/xilsecure.cmake
@@ -0,0 +1,7 @@
+
+option(XILSECURE_secure_environment "Enables trusted execution environment" OFF)
+if(XILSECURE_secure_environment)
+	set(XSECURE_TRUSTED_ENVIRONMENT " ")
+endif()
+
+configure_file(${CMAKE_CURRENT_SOURCE_DIR}/../data/xsecure_config.h.in ${CMAKE_BINARY_DIR}/include/xsecure_config.h)
