20e4d67bbc8016db37092a6eb9c38e621a0167f1
Appana Durga Kedareswara rao <appana.durga.rao@xilinx.com>
2021-02-23 18:35:24 +0530

    lwip_udp_perf_client: Add support for system device-tree flow
    Signed-off-by: Appana Durga Kedareswara rao <appana.durga.rao@xilinx.com>

--- lib/sw_apps/lwip_udp_perf_client/src/main.c
+++ lib/sw_apps/lwip_udp_perf_client/src/main.c
@@ -65,7 +65,7 @@ int ProgramSfpPhy(void);
 #endif
 
 #ifdef XPS_BOARD_ZCU102
-#ifdef XPAR_XIICPS_0_DEVICE_ID
+#ifdef XPAR_XIICPS_0_BASEADDR
 int IicPhyReset(void);
 #endif
 #endif
@@ -145,8 +145,7 @@ int main(void)
 	}
 	netif_set_default(netif);
 
-	/* now enable interrupts */
-	platform_enable_interrupts();
+	init_timer();
 
 	/* specify that the network if is up */
 	netif_set_up(netif);
