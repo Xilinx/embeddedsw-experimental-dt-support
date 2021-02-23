a9ad50ecfd69ad12cf49f4c725b860779093b9b2
Appana Durga Kedareswara rao <appana.durga.rao@xilinx.com>
2021-02-23 18:32:26 +0530

    lwip_tcp_perf_client: Add support for system device-tree flow
    Signed-off-by: Appana Durga Kedareswara rao <appana.durga.rao@xilinx.com>

--- lib/sw_apps/lwip_tcp_perf_client/src/platform.c
+++ lib/sw_apps/lwip_tcp_perf_client/src/platform.c
@@ -25,13 +25,10 @@
  * OF SUCH DAMAGE.
  *
  */
-#if __MICROBLAZE__
 #include "arch/cc.h"
 #include "platform.h"
 #include "platform_config.h"
 #include "xil_cache.h"
-#include "xparameters.h"
-#include "xintc.h"
 #include "xil_exception.h"
 #include "lwip/tcp.h"
 #ifdef STDOUT_IS_16550
@@ -39,6 +36,7 @@
 #endif
 
 #include "lwip/tcp.h"
+#include "xiltimer.h"
 
 #if LWIP_DHCP==1
 volatile int dhcp_timoutcntr = 24;
@@ -81,69 +79,17 @@ timer_callback()
 	}
 }
 
-static XIntc intc;
-
-void platform_setup_interrupts()
+void TimerCounterHandler(void *CallBackRef, u32 TmrCtrNumber)
 {
-	XIntc *intcp;
-	intcp = &intc;
-
-	XIntc_Initialize(intcp, XPAR_INTC_0_DEVICE_ID);
-	XIntc_Start(intcp, XIN_REAL_MODE);
-
-	/* Start the interrupt controller */
-	XIntc_MasterEnable(XPAR_INTC_0_BASEADDR);
-
-#ifdef __MICROBLAZE__
-	microblaze_register_handler((XInterruptHandler)XIntc_InterruptHandler, intcp);
-#endif
-
-	platform_setup_timer();
-
-#ifdef XPAR_ETHERNET_MAC_IP2INTC_IRPT_MASK
-	/* Enable timer and EMAC interrupts in the interrupt controller */
-	XIntc_EnableIntr(XPAR_INTC_0_BASEADDR,
-#ifdef __MICROBLAZE__
-			PLATFORM_TIMER_INTERRUPT_MASK |
-#endif
-			XPAR_ETHERNET_MAC_IP2INTC_IRPT_MASK);
-#endif
-
-
-#ifdef XPAR_INTC_0_LLTEMAC_0_VEC_ID
-#ifdef __MICROBLAZE__
-	XIntc_Enable(intcp, PLATFORM_TIMER_INTERRUPT_INTR);
-#endif
-	XIntc_Enable(intcp, XPAR_INTC_0_LLTEMAC_0_VEC_ID);
-#endif
-
-
-#ifdef XPAR_INTC_0_AXIETHERNET_0_VEC_ID
-	XIntc_Enable(intcp, PLATFORM_TIMER_INTERRUPT_INTR);
-	XIntc_Enable(intcp, XPAR_INTC_0_AXIETHERNET_0_VEC_ID);
-#endif
-
-
-#ifdef XPAR_INTC_0_EMACLITE_0_VEC_ID
-#ifdef __MICROBLAZE__
-	XIntc_Enable(intcp, PLATFORM_TIMER_INTERRUPT_INTR);
-#endif
-	XIntc_Enable(intcp, XPAR_INTC_0_EMACLITE_0_VEC_ID);
-#endif
-
-
+        timer_callback();
 }
 
 void
 enable_caches()
 {
 #ifdef __MICROBLAZE__
-#ifdef XPAR_MICROBLAZE_USE_ICACHE
 	Xil_ICacheEnable();
-#endif
-#ifdef XPAR_MICROBLAZE_USE_DCACHE
 	Xil_DCacheEnable();
-#endif
 #endif
 }
 
@@ -162,8 +108,13 @@ void init_platform()
 	XUartNs550_SetBaud(STDOUT_BASEADDR, XPAR_XUARTNS550_CLOCK_HZ, 9600);
 	XUartNs550_SetLineControlReg(STDOUT_BASEADDR, XUN_LCR_8_DATA_BITS);
 #endif
+}
 
-	platform_setup_interrupts();
+void init_timer()
+{
+	/* Calibrate the platform timer for 1 ms */
+	XTimer_SetInterval(1);
+	XTimer_SetHandler(TimerCounterHandler, 0);
 }
 
 void cleanup_platform()
@@ -173,7 +124,5 @@ void cleanup_platform()
 
 u64_t get_time_ms()
 {
-	return tickcntr * 10;
+	return tickcntr;
 }
-
-#endif
