2709d1299c7055e58ced9e142ee3a4142a4ea145
Appana Durga Kedareswara rao <appana.durga.rao@xilinx.com>
2021-04-06 20:05:18 +0530

    xilpm: src: versal: Fix IPI define checks for SDT flow
    Signed-off-by: Appana Durga Kedareswara rao <appana.durga.rao@xilinx.com>

--- lib/sw_services/xilpm/src/versal_common/server/xpm_ipi.h
+++ lib/sw_services/xilpm/src/versal_common/server/xpm_ipi.h
@@ -18,6 +18,14 @@ extern "C" {
 
 #define PM_IPI_TIMEOUT			(~0U)
 
+#ifdef XPAR_XIPIPSU_0_DEVICE_ID
+#define PSM_IPI_INT_MASK		XPAR_XIPIPS_TARGET_PSV_PSM_0_CH0_MASK
+#elif defined(SDT)
+#define PSM_IPI_INT_MASK		0x00000001U
+#else
+#define PSM_IPI_INT_MASK		(0U)
+#endif /* XPAR_XIPIPSU_0_DEVICE_ID */
+
 XStatus XPm_IpiSend(u32 IpiMask, u32 *Payload);
 XStatus XPm_IpiReadStatus(u32 IpiMask);
 XStatus XPm_IpiPollForAck(u32 IpiMask, u32 TimeOutCount);
