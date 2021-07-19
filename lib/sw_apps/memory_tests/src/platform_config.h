d9327f5902094671aec425e24d51e5ac6cd6d6e4
Appana Durga Kedareswara rao <appana.durga.rao@xilinx.com>
2021-07-19 19:34:38 +0530

    lib: sw_apps: Add copyright info for missing files
    This commit updates the copyright info for the missing files.
    
    Signed-off-by: Appana Durga Kedareswara rao <appana.durga.rao@xilinx.com>

--- lib/sw_apps/memory_tests/src/platform_config.h
+++ lib/sw_apps/memory_tests/src/platform_config.h
@@ -1,3 +1,8 @@
+/******************************************************************************
+* Copyright (c) 2021 Xilinx, Inc.  All rights reserved.
+* SPDX-License-Identifier: MIT
+*******************************************************************************/
+
 #ifndef __PLATFORM_CONFIG_H_
 #define __PLATFORM_CONFIG_H_
 
