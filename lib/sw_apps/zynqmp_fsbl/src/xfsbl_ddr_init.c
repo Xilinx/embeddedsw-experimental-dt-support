81d525e244113cfe97f4a59dd14029bf079f04b3
Appana Durga Kedareswara rao <appana.durga.rao@xilinx.com>
2021-09-17 11:24:03 +0530

    lib: sw_apps: zynqmp_fsbl: Don't pull IICPS related code if I2C instance is not present in the design
    If the design/system device-tree doesn't contain I2C instance,
    we shouldn't compile iicps driver api's.
    
    This commit updates the xfsbl_ddr_init.c code for the same.
    
    Signed-off-by: Appana Durga Kedareswara rao <appana.durga.rao@xilinx.com>

--- lib/sw_apps/zynqmp_fsbl/src/xfsbl_ddr_init.c
+++ lib/sw_apps/zynqmp_fsbl/src/xfsbl_ddr_init.c
@@ -43,7 +43,9 @@
 #ifdef XFSBL_PS_DDR
 #if defined(XPAR_DYNAMIC_DDR_ENABLED) || defined(XPAR_XDDRCPSU_0_DDRC_DYNAMIC_DDR_CONFIG_ENABLED)
 
+#if defined(XPAR_XIICPS_0_BASEADDR)
 #include "xiicps.h"
+#endif
 #include "xfsbl_ddr_init.h"
 
 /************************** Constant Definitions *****************************/
@@ -6636,6 +6638,7 @@ END:
  *			returns XFSBL_SUCCESS on success
  *
  *****************************************************************************/
+#if defined(XPAR_XIICPS_0_BASEADDR)
 static u32 XFsbl_IicReadSpdEeprom(u8 *SpdData)
 {
 	XIicPs IicInstance;		/* The instance of the IIC device. */
@@ -6845,6 +6848,7 @@ static u32 XFsbl_IicReadSpdEeprom(u8 *SpdData)
 END:
 	return UStatus;
 }
+#endif
 
 /*****************************************************************************/
 /**
@@ -6874,11 +6878,13 @@ u32 XFsbl_DdrInit(void)
 	};
 
 	/* Get the Model Part Number from the SPD stored in EEPROM */
+#if defined(XPAR_XIICPS_0_BASEADDR)
 	Status = XFsbl_IicReadSpdEeprom(SpdData);
 	if (Status != XFSBL_SUCCESS) {
 		Status = XFSBL_FAILURE;
 		goto END;
 	}
+#endif
 
 #if defined(XPS_BOARD_ZCU102) || defined(XPS_BOARD_ZCU106) \
 	|| defined(XPS_BOARD_ZCU111) || defined(XPS_BOARD_ZCU216) \
