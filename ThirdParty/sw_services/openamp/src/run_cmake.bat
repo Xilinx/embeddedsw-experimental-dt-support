96a14cb6ea275c607983d05d60604516be8d5483
Appana Durga Kedareswara rao <appana.durga.rao@xilinx.com>
2022-07-14 16:46:52 +0530

    esw: delete libmetal and openamp code as it is not ported for sdt flow
    Signed-off-by: Appana Durga Kedareswara rao <appana.durga.rao@xilinx.com>

--- ThirdParty/sw_services/openamp/src/run_cmake.bat
+++
@@ -1,8 +0,0 @@
-if not "%HDI_APPROOT%" == "" (
-%HDI_APPROOT%\tps\win64\cmake-3.3.2\bin\cmake.exe %* 2>&1
-) else (
-if "%XILINX_SDK%"== "" (
-%XILINX_VITIS%\tps\win64\cmake-3.3.2\bin\cmake.exe %* 2>&1
-) else (
-%XILINX_SDK%\tps\win64\cmake-3.3.2\bin\cmake.exe %* 2>&1
-))
