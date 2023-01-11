bfdd3032d0f3de59305bd2ecf6af8d996f8117ef
Appana Durga Kedareswara rao <appana.durga.kedareswara.rao@amd.com>
2023-01-11 14:23:50 +0530

    esw: Fix sleep.h pulling for yocto flow
    In yocto flow libxil get compiled first, few drivers
    expects sleep.h to be present but with latest code xiltimer
    is installing sleep.h which works for Vitis flow,To fix
    yocto build issue pull sleep.h from standalone bsp for yocto flow.
    
    Signed-off-by: Appana Durga Kedareswara rao <appana.durga.kedareswara.rao@amd.com>

--- lib/bsp/standalone/src/common/sleep.h
+++ lib/bsp/standalone/src/common/sleep.h
@@ -1,35 +1,22 @@
 /******************************************************************************
-* Copyright (c) 2014 - 2022 Xilinx, Inc.  All rights reserved.
+*
+* Copyright (C) 2021 Xilinx, Inc. All rights reserved.
 * SPDX-License-Identifier: MIT
+*
 ******************************************************************************/
-
 
 /*****************************************************************************/
 /**
 * @file sleep.h
 *
-*  This header file contains ARM Cortex A53,A9,R5,Microblaze specific sleep
-*  related APIs.
+*  This header file contains sleep related APIs.
 *
 * <pre>
 * MODIFICATION HISTORY :
 *
 * Ver   Who  Date	 Changes
 * ----- ---- -------- -------------------------------------------------------
-* 6.6   srm  11/02/17 Added processor specific sleep routines
-*								 function prototypes.
-* 7.7	sk   01/10/22 Typecast sleep declaration argument from unsigned int to
-* 		      u32 to fix misra_c_2012_directive_4_6 violation.
-* 7.7	sk   01/10/22 Modify the return type of sleep_R5 and usleep_R5 from
-* 		      unsigned to void to fix misra_c_2012_rule_17_7 violation.
-* 8.0	sk   03/02/22 Update usleep_R5 and usleep parameter types to fix misra_
-*		      c_2012_directive_4_6 violation.
-* 8.0	sk   03/17/22 Modify the return type of usleep_MB from int to void and
-*		      sleep_MB from unsigned to void to fix misra_c_2012_rule_
-*		      17_7 violation.
-* 8.0	sk   03/17/22 Modify sleep_MB parameter type from unsigned int to
-*		      u32 and usleep_MB parameter type from unsigned long to
-*		      ULONG to fix misra_c_2012_rule_4_6 violation.
+*  1.0  adk	 24/11/21 Initial release.
 *
 * </pre>
 *
@@ -87,16 +74,8 @@ extern "C" {
 	(timeout>0) ? 0 : -1;  \
  }  )
 
-void usleep(ULONG useconds);
-void sleep(u32 seconds);
-void usleep_R5(ULONG useconds);
-void sleep_R5(u32 seconds);
-void usleep_MB(ULONG useconds);
-void sleep_MB(u32 seconds);
-int usleep_A53(unsigned long useconds);
-unsigned sleep_A53(unsigned int seconds);
-int usleep_A9(unsigned long useconds);
-unsigned sleep_A9(unsigned int seconds);
+void usleep(unsigned long useconds);
+void sleep(unsigned int seconds);
 
 #ifdef __cplusplus
 }
