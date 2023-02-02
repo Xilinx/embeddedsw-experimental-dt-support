7fe20915194a98eabb34cf6d50a9e7fc2679b2ea
Appana Durga Kedareswara rao <appana.durga.kedareswara.rao@amd.com>
2023-02-02 15:48:12 +0530

    lib: sw_apps: dhrystone: Port dhrystone app into system device-tree flow
    Port dhrystone app into system device-tree flow.
    
    Signed-off-by: Appana Durga Kedareswara rao <appana.durga.kedareswara.rao@amd.com>

---
+++ lib/sw_apps/dhrystone/src/platform_config.h
@@ -0,0 +1,9 @@
+/******************************************************************************
+* Copyright (c) 2021 Xilinx, Inc.  All rights reserved.
+* SPDX-License-Identifier: MIT
+*******************************************************************************/
+
+#ifndef __PLATFORM_CONFIG_H_
+#define __PLATFORM_CONFIG_H_
+
+#endif
