7462e9650190fc5faf2fe1832ba69eaed81828a1
Appana Durga Kedareswara rao <appana.durga.kedareswara.rao@amd.com>
2023-03-06 16:13:18 +0530

    Revert "regression testing"
    This reverts commit 401b6fb1fb8192ab2edc14ae4338bff2e8104946.

--- scripts/rigel_scripts/settings.sh
+++ scripts/rigel_scripts/settings.sh
@@ -2,8 +2,7 @@ export XILINX_TOOLCHAIN=/proj/xbuilds/2023.1_daily_latest/installs/lin64/Vitis/2
 export PYTHON_VER="python-3.8.3"
 export CMAKE_VER="cmake-3.24.2"
 
-#export XBUILDS_LOPPER_PATH=${XILINX_TOOLCHAIN}/data/lopper
-export XBUILDS_LOPPER_PATH=/proj/xhdsswstaff/appanad/repos/decoupling_repo/lopper_artifacts
+export XBUILDS_LOPPER_PATH=${XILINX_TOOLCHAIN}/data/lopper
 export XBUILDS_CMAKE_PATH=${XILINX_TOOLCHAIN}/tps/lnx64/${CMAKE_VER}
 export XBUILDS_PYTHON_PATH=${XILINX_TOOLCHAIN}/tps/lnx64/${PYTHON_VER}
 
@@ -12,7 +11,7 @@ export LD_LIBRARY_PATH=${XBUILDS_PYTHON_PATH}/lib:${XBUILDS_CMAKE_PATH}/libs/Ubu
 export PATH=${XILINX_TOOLCHAIN}/bin:${XILINX_TOOLCHAIN}/gnu/microblaze/lin/bin:${XILINX_TOOLCHAIN}/gnu/arm/lin/bin:${XILINX_TOOLCHAIN}/gnu/aarch32/lin/gcc-arm-linux-gnueabi/bin:${XILINX_TOOLCHAIN}/gnu/aarch32/lin/gcc-arm-none-eabi/bin:${XILINX_TOOLCHAIN}/gnu/aarch64/lin/aarch64-none/bin:${XILINX_TOOLCHAIN}/gnu/armr5/lin/gcc-arm-none-eabi/bin:${XBUILDS_PYTHON_PATH}/bin:${XBUILDS_LOPPER_PATH}/bin:${XBUILDS_CMAKE_PATH}/bin:$PATH
 
 export PYTHONPATH=${XBUILDS_LOPPER_PATH}:$PYTHONPATH
-export OSF="True"
-export ESW_REPO=/proj/xhdsswstaff/appanad/repos/decoupling_repo/esw_sdt_repo/rebase_2023/embeddedsw-experimental-dt-support
-#export ESW_REPO="${XILINX_TOOLCHAIN}/data/embeddedsw-sdt"
+export OSF=""
+#"True"
+export ESW_REPO="${XILINX_TOOLCHAIN}/data/embeddedsw-sdt"
 export LOPPER_DTC_FLAGS="-b 0 -@"
