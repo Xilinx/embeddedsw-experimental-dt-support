84cecb9df6fbfebce8be54e986191018be679698
Mubin Sayyed <mubin.sayyed@amd.com>
2023-06-27 12:26:43 +0530

    scripts: settings.sh: Replace XILINX_TOOLCHAIN with XILINX_VITIS
    In VitisNG flow we are getting toolchain base path through
    XILINX_VITIS env variable exported by Vitis infrastructure.
    
    Replace XILINX_TOOLCHAIN with XILINX_VITIS, since we are using
    XILINX_VITIS to detect base path in create_bsp.py, with this
    base path detection works in cleaner way for script flow as
    well as VitisNG flow.
    
    Signed-off-by: Mubin Sayyed <mubin.sayyed@amd.com>

--- scripts/rigel_scripts/settings.sh
+++ scripts/rigel_scripts/settings.sh
@@ -1,16 +1,17 @@
-export XILINX_TOOLCHAIN=/proj/xbuilds/2023.2_daily_latest/installs/lin64/Vitis/2023.2
+export XILINX_VITIS=/proj/xbuilds/2023.2_daily_latest/installs/lin64/Vitis/2023.2
+
 export PYTHON_VER="python-3.8.3"
 export CMAKE_VER="cmake-3.24.2"
 
-export XBUILDS_LOPPER_PATH=${XILINX_TOOLCHAIN}/data/lopper
-export XBUILDS_CMAKE_PATH=${XILINX_TOOLCHAIN}/tps/lnx64/${CMAKE_VER}
-export XBUILDS_PYTHON_PATH=${XILINX_TOOLCHAIN}/tps/lnx64/${PYTHON_VER}
+export XBUILDS_LOPPER_PATH=${XILINX_VITIS}/data/lopper
+export XBUILDS_CMAKE_PATH=${XILINX_VITIS}/tps/lnx64/${CMAKE_VER}
+export XBUILDS_PYTHON_PATH=${XILINX_VITIS}/tps/lnx64/${PYTHON_VER}
 
 # LD_LIBRARY_PATH is needed to add the linker libraries (e.g. libffi) to the path
 export LD_LIBRARY_PATH=${XBUILDS_PYTHON_PATH}/lib:${XBUILDS_CMAKE_PATH}/libs/Ubuntu:${LD_LIBRARY_PATH}
 
 # Source Paths of different toolchains, python, cmake and lopper binary
-export PATH=${XILINX_TOOLCHAIN}/bin:${XILINX_TOOLCHAIN}/gnu/microblaze/lin/bin:${XILINX_TOOLCHAIN}/gnu/arm/lin/bin:${XILINX_TOOLCHAIN}/gnu/aarch32/lin/gcc-arm-linux-gnueabi/bin:${XILINX_TOOLCHAIN}/gnu/aarch32/lin/gcc-arm-none-eabi/bin:${XILINX_TOOLCHAIN}/gnu/aarch64/lin/aarch64-none/bin:${XILINX_TOOLCHAIN}/gnu/armr5/lin/gcc-arm-none-eabi/bin:${XBUILDS_PYTHON_PATH}/bin:${XBUILDS_LOPPER_PATH}/bin:${XBUILDS_CMAKE_PATH}/bin:$PATH
+export PATH=${XILINX_VITIS}/bin:${XILINX_VITIS}/gnu/microblaze/lin/bin:${XILINX_VITIS}/gnu/arm/lin/bin:${XILINX_VITIS}/gnu/aarch32/lin/gcc-arm-linux-gnueabi/bin:${XILINX_VITIS}/gnu/aarch32/lin/gcc-arm-none-eabi/bin:${XILINX_VITIS}/gnu/aarch64/lin/aarch64-none/bin:${XILINX_VITIS}/gnu/armr5/lin/gcc-arm-none-eabi/bin:${XBUILDS_PYTHON_PATH}/bin:${XBUILDS_LOPPER_PATH}/bin:${XBUILDS_CMAKE_PATH}/bin:$PATH
 
 # Adds Installed Lopper directory to the PYTHONPATH
 export PYTHONPATH=${XBUILDS_LOPPER_PATH}:$PYTHONPATH
@@ -22,7 +23,7 @@ export VALIDATE_ARGS="True"
 export OSF="False"
 
 # Below is a fallback mechanism to set an esw repo when no user repo is set
-# export ESW_REPO="${XILINX_TOOLCHAIN}/data/embeddedsw-sdt"
+# export ESW_REPO="${XILINX_VITIS}/data/embeddedsw-sdt"
 
 # LOPPER_DTC_FLAGS is needed to generate device tree with symbols in it.
 export LOPPER_DTC_FLAGS="-b 0 -@"
